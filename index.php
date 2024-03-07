761affc6f8f3bc0b1ca8ee7ca9defb7f49038afc
chrisdk88 <chris_dk88@hotmail.com>
2024-03-07 12:57:18 +0100

    new

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+<?php
+session_start();
+
+$response = ['loggedIn' => false];
+
+// Opret forbindelse til databasen
+$mysqli = new mysqli('localhost', 'root', '', 'festival');
+if ($mysqli->connect_error) {
+    die('Connection Error: ' . $mysqli->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    header('Content-Type: application/json');
+    $data = json_decode(file_get_contents('php://input'), true);
+    $action = $data['action'] ?? '';
+
+    if ($action == 'login') {
+        $username = $data['username'] ?? '';
+        $password = $data['password'] ?? ''; // I det virkelige liv, bør passwordet hashes
+
+        $stmt = $mysqli->prepare('SELECT * FROM admin WHERE username = ?');
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $admin = $result->fetch_assoc();
+
+        if ($admin && $admin['password'] === $password) {
+            $_SESSION['admin_logged_in'] = true;
+            $_SESSION['username'] = $admin['username'];
+            $response['loggedIn'] = true;
+            $response['username'] = $admin['username'];
+        }
+        $stmt->close();
+        echo json_encode($response);
+        exit();
+    } elseif ($action == 'logout') {
+        session_destroy();
+        echo json_encode(['loggedIn' => false]);
+        exit();
+    }
+} elseif (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in']) {
+    $response['loggedIn'] = true;
+    $response['username'] = $_SESSION['username'];
+}
+
+$mysqli->close();
+?>
+
 <!DOCTYPE html>
 <html lang="da">
 <head>
@@ -10,24 +58,33 @@
   <header>
     <nav class="navbar">
       <div class="navbar-brand">
-        <img src="images/logo.png" alt="Logo">
+        <img src="images/logo.png" alt="Logo" data-page="home" id="logo">
       </div>
       <div class="navbar-title">
         <h1>Festival ting</h1>
       </div>
       <div class="navbar-form">
-        <form action="#" method="post">
-          <div class="form-group">
-            <input type="text" name="username" id="username" placeholder="Username">
-          </div>
-          <div class="form-group">
-            <input type="password" id="password" name="password" placeholder="Password">
-          </div>
-          <div class="form-group">
-            <input type="submit"id="loginbtn" value="Login">
-          </div>
+    <?php if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in']): ?>
+        <div class="welcome-message">
+            <p>Velkommen, <?php echo htmlspecialchars($_SESSION['username']); ?>!</p>
+        </div>
+        <form action="index.php" method="post">
+            <input type="hidden" name="action" value="logout">
+            <input type="submit" value="Log ud">
         </form>
-      </div>
+    <?php else: ?>
+        <form action="index.php" method="post">
+            <input type="hidden" name="action" value="login">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" value="Login" data-page="login" id="loginbtn">
+            <a href="#" data-page="login">login</a>
+        </form>
+        <?php if ($login_error): ?>
+            <p><?php echo $login_error; ?></p>
+        <?php endif; ?>
+    <?php endif; ?>
+</div>
     </nav>
   </header>
 
@@ -43,9 +100,66 @@
     <div id="content">
       <h1>Virker dette?</h1>
       <p>Her finder du de bedste festivals</p>
+      <div class="box-container">
+    <div class="box">
+      <img src="john.jpg" alt="Example Image">
+      <button class="comment-button">Kommentar</button>
+ 
+      <div class="box-info">
+        <h2>Information Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora exercitationem suscipit quibusdam, ad eius eum modi repudiandae, alias voluptas, provident doloribus libero? Sequi sapiente quas excepturi debitis error reprehenderit labore non odio similique quos ipsa quisquam obcaecati aut, quibusdam dolores vero at aliquam nulla in rem sunt, modi nihil? Ullam cum officiis tempore eligendi iste, accusamus voluptatibus quae nisi eum velit deleniti placeat, consequatur officia fugiat numquam, similique est ea soluta aperiam repudiandae aspernatur. Debitis saepe voluptatem asperiores harum molestias, doloremque eaque, nobis modi minima voluptatibus voluptatum culpa optio, soluta reprehenderit obcaecati ab? Pariatur aliquid placeat nam maxime veniam aspernatur doloremque repellendus sapiente doloribus asperiores modi esse iusto, facere exercitationem earum voluptate perspiciatis rerum recusandae repudiandae vel ea nisi. Hic, et molestiae vitae distinctio doloribus nulla ex harum quas ratione quae voluptas eveniet nihil rem impedit facere eaque ut quos ipsam? Quo, porro explicabo. Vitae libero blanditiis similique eligendi optio modi quae natus suscipit maiores incidunt minima repudiandae veniam nisi vel, inventore necessitatibus laborum voluptate deserunt esse. Molestiae rem itaque quisquam, assumenda doloribus commodi modi! Cumque sapiente illum voluptatum dicta ut soluta laboriosam, expedita,</p>
+      </div>
+      <div class="rating">
+      <input type="radio" id="star5" name="rating" value="5"><label for="star5"></label>
+      <input type="radio" id="star4" name="rating" value="4"><label for="star4"></label>
+      <input type="radio" id="star3" name="rating" value="3"><label for="star3"></label>
+      <input type="radio" id="star2" name="rating" value="2"><label for="star2"></label>
+      <input type="radio" id="star1" name="rating" value="1"><label for="star1"></label>
+    </div>
+    </div>
+  </div>
+  <div class="box-container">
+    <div class="box">
+      <img src="john.jpg" alt="Example Image">
+      <button class="comment-button">Kommentar</button>
+ 
+      <div class="box-info">
+        <h2>Information Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora exercitationem suscipit quibusdam, ad eius eum modi repudiandae, alias voluptas, provident doloribus libero? Sequi sapiente quas excepturi debitis error reprehenderit labore non odio similique quos ipsa quisquam obcaecati aut, quibusdam dolores vero at aliquam nulla in rem sunt, modi nihil? Ullam cum officiis tempore eligendi iste, accusamus voluptatibus quae nisi eum velit deleniti placeat, consequatur officia fugiat numquam, similique est ea soluta aperiam repudiandae aspernatur. Debitis saepe voluptatem asperiores harum molestias, doloremque eaque, nobis modi minima voluptatibus voluptatum culpa optio, soluta reprehenderit obcaecati ab? Pariatur aliquid placeat nam maxime veniam aspernatur doloremque repellendus sapiente doloribus asperiores modi esse iusto, facere exercitationem earum voluptate perspiciatis rerum recusandae repudiandae vel ea nisi. Hic, et molestiae vitae distinctio doloribus nulla ex harum quas ratione quae voluptas eveniet nihil rem impedit facere eaque ut quos ipsam? Quo, porro explicabo. Vitae libero blanditiis similique eligendi optio modi quae natus suscipit maiores incidunt minima repudiandae veniam nisi vel, inventore necessitatibus laborum voluptate deserunt esse. Molestiae rem itaque quisquam, assumenda doloribus commodi modi! Cumque sapiente illum voluptatum dicta ut soluta laboriosam, expedita,</p>
+      </div>
+      <div class="rating">
+      <input type="radio" id="star5" name="rating" value="5"><label for="star5"></label>
+      <input type="radio" id="star4" name="rating" value="4"><label for="star4"></label>
+      <input type="radio" id="star3" name="rating" value="3"><label for="star3"></label>
+      <input type="radio" id="star2" name="rating" value="2"><label for="star2"></label>
+      <input type="radio" id="star1" name="rating" value="1"><label for="star1"></label>
+    </div>
+    </div>
+  </div>
+  <div class="ad">
+</div>
+  <div class="box-container">
+    <div class="box">
+      <img src="john.jpg" alt="Example Image">
+      <button class="comment-button">Kommentar</button>
+ 
+      <div class="box-info">
+        <h2>Information Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora exercitationem suscipit quibusdam, ad eius eum modi repudiandae, alias voluptas, provident doloribus libero? Sequi sapiente quas excepturi debitis error reprehenderit labore non odio similique quos ipsa quisquam obcaecati aut, quibusdam dolores vero at aliquam nulla in rem sunt, modi nihil? Ullam cum officiis tempore eligendi iste, accusamus voluptatibus quae nisi eum velit deleniti placeat, consequatur officia fugiat numquam, similique est ea soluta aperiam repudiandae aspernatur. Debitis saepe voluptatem asperiores harum molestias, doloremque eaque, nobis modi minima voluptatibus voluptatum culpa optio, soluta reprehenderit obcaecati ab? Pariatur aliquid placeat nam maxime veniam aspernatur doloremque repellendus sapiente doloribus asperiores modi esse iusto, facere exercitationem earum voluptate perspiciatis rerum recusandae repudiandae vel ea nisi. Hic, et molestiae vitae distinctio doloribus nulla ex harum quas ratione quae voluptas eveniet nihil rem impedit facere eaque ut quos ipsam? Quo, porro explicabo. Vitae libero blanditiis similique eligendi optio modi quae natus suscipit maiores incidunt minima repudiandae veniam nisi vel, inventore necessitatibus laborum voluptate deserunt esse. Molestiae rem itaque quisquam, assumenda doloribus commodi modi! Cumque sapiente illum voluptatum dicta ut soluta laboriosam, expedita,</p>
+      </div>
+      <div class="rating">
+      <input type="radio" id="star5" name="rating" value="5"><label for="star5"></label>
+      <input type="radio" id="star4" name="rating" value="4"><label for="star4"></label>
+      <input type="radio" id="star3" name="rating" value="3"><label for="star3"></label>
+      <input type="radio" id="star2" name="rating" value="2"><label for="star2"></label>
+      <input type="radio" id="star1" name="rating" value="1"><label for="star1"></label>
+    </div>
+    </div>
+  </div>
     </div>
   </div>
 
+  <!-- This script is only for testing how changing innerhtml it working -->
   <script src="javascript/script.js"></script>
 </body>
 </html>
--- javascript/script.js
+++ javascript/script.js
@@ -15,14 +15,95 @@ document.addEventListener("DOMContentLoaded", function () {
         content.innerHTML =
           "<h1>Kontakt</h1><p>Her finder du de bedste festivals</p>";
         break;
+      case "login":
+        // Tjek sessionen for at afgøre, om brugeren er logget ind
+        fetch("check_session.php") // check_session.php er en PHP-fil, der tjekker sessionens tilstand
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.loggedIn) {
+              content.innerHTML = `
+                  <div class="welcome-message">
+                    <p>Velkommen, ${data.username}!</p>
+                  </div>
+                  <form action="index.php" method="post">
+                    <input type="hidden" name="action" value="logout">
+                    <input type="submit" value="Log ud">
+                  </form>`;
+            } else {
+              content.innerHTML = `
+                  <h1 id="textLogin">Admin login</h1>
+                  <form id="loginForm2">
+                    <div class="form-group2">
+                      <label for="loginUsername">Brugernavn:</label>
+                      <input type="text" id="loginUsername" name="username" placeholder="Indtast brugernavn">
+                    </div>
+                    <div class="form-group2">
+                      <label for="loginPassword">Adgangskode:</label>
+                      <input type="password" id="loginPassword" name="password" placeholder="Indtast adgangskode">
+                    </div>
+                    <div class="form-group2">
+                      <button onclick="submitLogin(); return false;">Log ind</button>
+                    </div>
+                  </form>`;
+            }
+          });
+        break;
       default:
         content.innerHTML =
           "<h1>Velkommen tilbage!</h1><p>Vælg noget fra navbaren.</p>";
     }
   }
 
-  // Listen to clicks on navigation links
-  document.querySelectorAll("ul li a").forEach((link) => {
+  function submitLogin() {
+    var username = document.getElementById("loginUsername").value;
+    var password = document.getElementById("loginPassword").value;
+
+    fetch("index.php", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        action: "login",
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.loggedIn) {
+          document.getElementById(
+            "content"
+          ).innerHTML = `Velkommen, ${data.username}! <button onclick="logout()">Log ud</button>`;
+        } else {
+          document.getElementById("content").innerHTML =
+            "Login fejlede. Prøv igen.";
+        }
+      });
+  }
+
+  function logout() {
+    fetch("index.php", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ action: "logout" }),
+    }).then(() => {
+      // Opdater siden eller vis loginform igen
+      showLoginForm(); // Du skal implementere denne funktion
+    });
+  }
+
+  // This function will change the content div
+  function addClickListener(elementId, page) {
+    document.getElementById(elementId).addEventListener("click", function (e) {
+      e.preventDefault();
+      updateContent(page);
+    });
+  }
+
+  addClickListener("logo", "home");
+  addClickListener("loginbtn", "login");
+
+  // Listen to clicks on all a tags
+  document.querySelectorAll("a").forEach((link) => {
     link.addEventListener("click", function (e) {
       e.preventDefault();
       const page = this.getAttribute("data-page");
