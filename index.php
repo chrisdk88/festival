4c42a2a8f844a2c44f2838bfe950b6319fa3128b
chrisdk88 <chris_dk88@hotmail.com>
2024-03-11 10:16:29 +0100

    Updated

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
 session_start();
 
-$response = ['loggedIn' => false];
-
-// Opret forbindelse til databasen
+// Create connection to database
 $mysqli = new mysqli('localhost', 'root', '', 'festival');
 if ($mysqli->connect_error) {
     die('Connection Error: ' . $mysqli->connect_error);
 }
 
+$response = ['loggedIn' => false];
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    header('Content-Type: application/json');
-    $data = json_decode(file_get_contents('php://input'), true);
+    // header('Content-Type: application/json');
+    $data = $_POST;
     $action = $data['action'] ?? '';
 
     if ($action == 'login') {
+        error_log("Login action triggered");
+        echo "Login action triggered";
         $username = $data['username'] ?? '';
         $password = $data['password'] ?? ''; 
 
@@ -29,22 +31,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_SESSION['username'] = $admin['username'];
             $response['loggedIn'] = true;
             $response['username'] = $admin['username'];
+        } else {
+            $response['loggedIn'] = false;
+            $response['error'] = 'Login fejlede. Forkert brugernavn eller adgangskode.';
         }
         $stmt->close();
         echo json_encode($response);
         exit();
     } elseif ($action == 'logout') {
         session_destroy();
-        echo json_encode(['loggedIn' => false]);
+        $response = ['loggedIn' => false]; 
+        echo json_encode($response);
         exit();
-    }
-} elseif (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in']) {
+    } elseif ($action == 'checkLogin') {
+      // This checks the session if the user is already logged in
+      if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in']) {
+          $response['loggedIn'] = true;
+          $response['username'] = $_SESSION['username'];
+      } else {
+          $response['loggedIn'] = false;
+      }
+      echo json_encode($response);
+      exit();
+  }
+}
+
+// This checks the session if the user is already logged in
+if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in']) {
     $response['loggedIn'] = true;
     $response['username'] = $_SESSION['username'];
 }
 
+// This could be placed here to handle scenarios where no POST request has been made, such as during direct page loading.
+echo json_encode($response);
 $mysqli->close();
 ?>
+
 
 <!DOCTYPE html>
 <html lang="da">
@@ -53,6 +75,8 @@ $mysqli->close();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Festival</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
 </head>
 <body>
   <header>
@@ -66,23 +90,15 @@ $mysqli->close();
       <div class="navbar-form">
     <?php if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in']): ?>
         <div class="welcome-message">
-            <p>Velkommen, <?php echo htmlspecialchars($_SESSION['username']); ?>!</p>
+            <p>Velkommen, <?= htmlspecialchars($_SESSION['username']); ?>!</p>
         </div>
         <form action="index.php" method="post">
-            <input type="hidden" name="action" value="logout">
-            <input type="submit" value="Log ud">
+          <a href="#" data-page="login" id="loginbtn">logud</a>
         </form>
     <?php else: ?>
-        <form action="index.php" method="post">
-            <input type="hidden" name="action" value="login">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" value="Login" data-page="login" id="loginbtn">
-            <a href="#" data-page="login">login</a>
+        <form action="index.php" method="post">          
+          <a href="#" data-page="login" id="loginbtn">login</a>
         </form>
-        <?php if ($login_error): ?>
-            <p><?php echo $login_error; ?></p>
-        <?php endif; ?>
     <?php endif; ?>
 </div>
     </nav>
@@ -104,7 +120,7 @@ $mysqli->close();
     <div class="box">
       <img src="images/logo.png" alt="Example Image">
       <button class="comment-button">Kommentar</button>
- 
+
       <div class="box-info">
         <h2>Information Title</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora exercitationem suscipit quibusdam, ad eius eum modi repudiandae, alias voluptas, provident doloribus libero? Sequi sapiente quas excepturi debitis error reprehenderit labore non odio similique quos ipsa quisquam obcaecati aut, quibusdam dolores vero at aliquam nulla in rem sunt, modi nihil? Ullam cum officiis tempore eligendi iste, accusamus voluptatibus quae nisi eum velit deleniti placeat, consequatur officia fugiat numquam, similique est ea soluta aperiam repudiandae aspernatur. Debitis saepe voluptatem asperiores harum molestias, doloremque eaque, nobis modi minima voluptatibus voluptatum culpa optio, soluta reprehenderit obcaecati ab? Pariatur aliquid placeat nam maxime veniam aspernatur doloremque repellendus sapiente doloribus asperiores modi esse iusto, facere exercitationem earum voluptate perspiciatis rerum recusandae repudiandae vel ea nisi. Hic, et molestiae vitae distinctio doloribus nulla ex harum quas ratione quae voluptas eveniet nihil rem impedit facere eaque ut quos ipsam? Quo, porro explicabo. Vitae libero blanditiis similique eligendi optio modi quae natus suscipit maiores incidunt minima repudiandae veniam nisi vel, inventore necessitatibus laborum voluptate deserunt esse. Molestiae rem itaque quisquam, assumenda doloribus commodi modi! Cumque sapiente illum voluptatum dicta ut soluta laboriosam, expedita,</p>
@@ -159,7 +175,6 @@ $mysqli->close();
     </div>
   </div>
 
-  <!-- This script is only for testing how changing innerhtml it working -->
   <script src="javascript/script.js"></script>
 </body>
 </html>
